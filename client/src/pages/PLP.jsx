46d15362b8fa388e51c03ddd678e9b64882dfe98
phuongg312 <lethithuphuong03122004@gmail.com>
2025-10-08 09:47:18 +0700

    ver-4

--- client/src/pages/PLP.jsx
+++ client/src/pages/PLP.jsx
@@ -2,17 +2,17 @@ import CategoryHeading from "../components/CategoryHeading";
 import Resources from "../components/Resources";
 import UpsellCarousel from "../components/UpsellCarousel";
 import RecentlyViewed from "../components/RecentlyViewed";
-import KV from "../components/KV";
+import KV from "../components/kv";
 import Product from "../components/Product";
 export default function PLP() {
   return (
     <>
-     <CategoryHeading/>
-     <Product/>
-     <KV/>
-     <RecentlyViewed/>
-     <UpsellCarousel/>
-     <Resources/>
+      <CategoryHeading />
+      <Product />
+      <KV />
+      <RecentlyViewed />
+      <UpsellCarousel />
+      <Resources />
     </>
   );
 }
