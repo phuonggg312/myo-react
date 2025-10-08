dce7df1eb7c513ce26ffc8b859e4d1fe688b23ea
phuongg312 <lethithuphuong03122004@gmail.com>
2025-10-08 09:53:22 +0700

    ver-4.1

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/images/Logo-1.png";
+import logo from "../assets/images/logo-1.png";
 
 export default function Footer() {
   return (
